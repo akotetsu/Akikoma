55cc08700f01255025110bd293b3b006eb8c19c0
akotetsu <yuanliqu@gmail.com>
2025-08-10 23:46:31 +0900

    Initial Commit

---
+++ Akikoma/AkikomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkikomaApp.swift
+//  Akikoma
+//
+//  Created by 原里駆 on 2025/08/10.
+//
+
+import SwiftUI
+
+@main
+struct AkikomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
